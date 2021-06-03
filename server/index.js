dccfec2e12736d1d8bd400b0e0fcee4766ca170a
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-06-03 22:07:40 +0300

    routers and controllers on server side

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
 app.use("/posts", postRoutes);
 
 //https://www.mongodb.com/cloud/atlas/
