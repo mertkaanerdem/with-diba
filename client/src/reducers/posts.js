c172d64b29b8a8827f4fc9039d0f33656d1e85e7
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-06-03 22:08:02 +0300

    reducers

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,12 @@
+export default function reducer(posts = [], action) {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return action.payload;
+
+    case "CREATE":
+      return [...posts, action.payload];
+
+    default:
+      return posts;
+  }
+}
