418fe6d1b3e7a1ebfaa01bcdad799e2517342c72
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-14 23:20:09 +0300

    auth middleware

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,14 +6,15 @@ import {
   deletePost,
   likePost,
 } from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", createPosts);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPosts); //auth dediği auth middleware den geliyor. auth olmadan post yaratılamaz
+router.patch("/:id", auth, updatePost); //auth dediği auth middleware den geliyor. auth olmadan post güncellenemez
+router.delete("/:id", auth, deletePost); //auth dediği auth middleware den geliyor. auth olmadan post silinemez
+router.patch("/:id/likePost", auth, likePost); //auth dediği auth middleware den geliyor. auth olmadan post beğenilemez
 export default router;
 
 //http://localhost:5000/posts
