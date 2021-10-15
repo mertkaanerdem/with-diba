c4c907b2d84f3c12189fc68758b1290122c64f2c
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-10-16 00:12:24 +0300

    user deleting post function

--- client/src/components/Posts/PostCard/PostCard.js
+++ client/src/components/Posts/PostCard/PostCard.js
@@ -81,22 +81,25 @@ export default function PostCard({ posts, setCurrentId }) {
             <Likes />
           </button>
 
-          <button onClick={() => dispatch(deletePost(posts._id))}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 hover:text-yellow-500 duration-400 cursor-pointer "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
-          </button>
+          {(user?.result?.googleId === posts?.creator ||
+            user?.result?._id === posts?.creator) && (
+            <button onClick={() => dispatch(deletePost(posts._id))}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:text-yellow-500 duration-400 cursor-pointer "
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
+          )}
         </div>
       </div>
     </div>
